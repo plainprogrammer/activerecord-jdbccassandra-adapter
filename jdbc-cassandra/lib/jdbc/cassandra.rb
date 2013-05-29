e267c9bb5a33863f69c1650f9b7b012b7e7c25e3
James Thompson <jamest@thereadyproject.com>
2013-05-29 17:00:56 -0600

    Added dependencies for CassandraDriver.

--- jdbc-cassandra/lib/jdbc/cassandra.rb
+++ jdbc-cassandra/lib/jdbc/cassandra.rb
@@ -1,13 +1,16 @@
 warn "Jdbc-Cassandra is only for use with JRuby" if (JRUBY_VERSION.nil? rescue true)
 require 'jdbc/cassandra/version'
 
+require 'cassandra-clientutil-1.2.5.jar'
+require 'cassandra-thrift-1.2.5.jar'
+
 module Jdbc
   module Cassandra
     def self.driver_jar
       "cassandra-jdbc-#{DRIVER_VERSION}.jar"
     end
 
-    def self.load_driver(method = :load)
+    def self.load_driver(method = :require)
       send method, driver_jar
     end
 
